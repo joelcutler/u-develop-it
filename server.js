006427b7710790c3d7c1ec1154622bb43e3a40ff
Joel C <joel7921111@yahoo.com>
2022-04-05 18:27:25 -0600

    added query to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +8,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-// catch-all route- MUST BE THE LAST ROUTE: This route will override all others
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'm242803m!MYSQL',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
+// catch-all route- MUST BE THE LAST ROUTE: This route will override all others
 app.use((eq, res) => {
     res.status(404).end();
 });
